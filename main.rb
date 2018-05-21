2325b8696c8fd99aff07c2bc84d7a00a0a25159a
Valerio Battaglia <vabatta@gmail.com>
2018-05-21 21:29:42 +0200

    Fixed the main.rb script

--- main.rb
+++ main.rb
@@ -2,9 +2,10 @@
 load "runner.rb"
 
 keepass_server = "http://localhost:19455"
-key_name = "08867FDB-AE60-4361-9425-A1A07A9B5E45"
-aes_key = "5xXezHcnq8HbxMCUyPu9g3AiryOscUxWjkrOH8VdzGo="
+key_name = "<YOUR KEY NAME>"
+aes_key = "<YOUR AES KEY>"
+format = "%s %s"
 url = ARGV[0]
 # url = "{query}"
 
-puts run(keepass_server, key_name, aes_key, url)
+puts run(keepass_server, key_name, aes_key, format, url)
